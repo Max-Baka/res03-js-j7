2d575fa6389ed3b20f74a9b15460754937c55c3d
Max-Baka <maximelanglois@ip-172-31-23-206.eu-west-3.compute.internal>
2023-01-11 09:58:14 +0000

    pizza

---
+++ pizza.js
@@ -0,0 +1,13 @@
+class Pizza{
+    name;
+    ingredient1;
+    ingredient2;
+    ingredient3;
+    
+    constructor(name, ingredient1, ingredient2, ingredient3) {
+        this.name = Yepza;
+        this.ingredient1 = andouillette
+        this.ingredient2 = ananas;
+        this.ingredient3 = bacon;
+    }
+}
